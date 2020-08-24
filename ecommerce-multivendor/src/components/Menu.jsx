f85156748844c10234cfa1678ccaa0af501d55d0
ravininganiya <ry4raviyadav.com>
2020-08-24 12:51:52 +0530

    commit

---
+++ ecommerce-multivendor/src/components/Menu.jsx
@@ -0,0 +1,145 @@
+import React, { useState, useEffect } from "react";
+import {
+  Layout,
+  Menu,
+  Dropdown,
+  Button,
+  message,
+  Tooltip,
+  Divider,
+  Popconfirm,
+  Badge,
+} from "antd";
+import {
+  PhoneOutlined,
+  MailOutlined,
+  UserOutlined,
+  ToolOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+
+import history from "../history";
+import { useSelector, useDispatch, connect } from "react-redux";
+import { logoutUser } from "../actions/authActions";
+import { withCookies } from "react-cookie";
+import NavbarIndex from "./navbar/navbarIndex";
+
+const MenuIndex = (props) => {
+  const { Header, Content, Footer } = Layout;
+  const dispatch = useDispatch();
+
+  //console.log("props.userAuth", props.user.userName);
+
+  const onLogOutUser = () => {
+    const { cookies } = props;
+    cookies.remove("Authorization", { path: "/" });
+    cookies.remove("isSignedIn", { path: "/" });
+    cookies.remove("userId", { path: "/" });
+    cookies.remove("userType", { path: "/" });
+    cookies.remove("userName", { path: "/" });
+
+    // dispatch(logoutUser());
+    props.logoutUser();
+  };
+
+  const userMenu = (
+    <Menu
+    // onClick={handleMenuClick}
+    >
+      <Menu.Item key="1">
+        <ToolOutlined className="menu-item-icon" /> My Account
+      </Menu.Item>
+      <Menu.Item key="2">
+        <Popconfirm
+          title="Are you sure you want log out ?"
+          okText="Yes"
+          cancelText="No"
+          onConfirm={onLogOutUser}
+        >
+          <LogoutOutlined className="menu-item-icon" /> Sign out
+        </Popconfirm>
+      </Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <div>
+      <Layout style={{ background: "#fff" }}>
+        <Header
+          className="header"
+          style={{ position: "fixed", zIndex: 12345, width: "100%" }}
+        >
+          <div style={{ display: "flex" }}>
+            <div style={{ width: "48%" }}>
+              <span>
+                <PhoneOutlined className="menu-item-icon" />
+                <span className="top-menu-items"> Hotline : 1234-567-899</span>
+              </span>
+              <span style={{ marginLeft: "15px" }}>
+                <MailOutlined className="menu-item-icon" />
+                <span className="top-menu-items">
+                  Email : support@domain.com
+                </span>
+              </span>
+            </div>
+            <div style={{ width: "48%" }}>
+              <Dropdown.Button
+                style={{ float: "right" }}
+                overlay={userMenu}
+                placement="bottomCenter"
+                icon={<UserOutlined />}
+              >
+                <Badge color="#87d068" />
+                {props.user.userName ? props.user.userName : "My Account"}
+              </Dropdown.Button>
+            </div>
+          </div>
+        </Header>
+        <Header
+          style={{
+            //  position: "fixed",
+            //  top: "48px",
+            marginTop: 48,
+            width: "100%",
+            background: "#FFFFFF",
+          }}
+        >
+          <NavbarIndex />
+        </Header>
+        <Content
+          className="site-layout"
+          style={{
+            padding: "0 50px",
+            marginTop: 48,
+            background: "#E5EFF4",
+          }}
+        >
+          <div
+            style={{
+              height: "100vh",
+              //  border: "1px solid red"
+            }}
+          >
+            {props.children}
+          </div>
+        </Content>
+        <Footer
+          style={{ textAlign: "center", background: "#45AB67", color: "#fff" }}
+        >
+          videsh gujjar Design ©2020 Created by unprofessional
+          developers(videsh,manish)
+        </Footer>
+      </Layout>
+    </div>
+  );
+};
+
+//export default MenuIndex;
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.userAuth,
+  };
+};
+
+export default withCookies(connect(mapStateToProps, { logoutUser })(MenuIndex));
---
+++ ecommerce-multivendor/src/components/navbar/navbarIndex.jsx
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from "react";
+import { Menu } from "antd";
+import {
+  MailOutlined,
+  AppstoreOutlined,
+  SettingOutlined,
+  ShoppingCartOutlined,
+  CaretDownOutlined,
+} from "@ant-design/icons";
+
+const { SubMenu } = Menu;
+
+const NavbarIndex = (props) => {
+  return (
+    <div
+      className="menuBar"
+      style={{
+        width: "80%",
+        display: "flex",
+        background: "#fff",
+        textAlign: "center",
+        margin: "auto",
+        // marginLeft: "7%",
+      }}
+    >
+      <div className="Logo" style={{ width: "10%" }}>
+        <img
+          src="https://winmagictoys.com/wp-content/uploads/2018/09/dummy-logo.png"
+          width="80px"
+          alt="logo"
+        />
+      </div>
+      <div className="menuItems" style={{ width: "80%" }}>
+        <Menu mode="horizontal">
+          <Menu.Item key="mail" icon={<MailOutlined />}>
+            <a
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "#45ab67", fontSize: "1.2em" }}
+            >
+              Home
+            </a>
+          </Menu.Item>
+          <Menu.Item key="app">
+            <a
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "#45ab67", fontSize: "1.2em" }}
+            >
+              Pages
+            </a>
+          </Menu.Item>
+          <SubMenu
+            title="Category"
+            icon={
+              <CaretDownOutlined
+                style={{ color: "#45ab67", fontSize: "1.2em" }}
+              />
+            }
+          >
+            <Menu.ItemGroup title="Item 1">
+              <Menu.Item key="setting:1">Option 1</Menu.Item>
+              <Menu.Item key="setting:2">Option 2</Menu.Item>
+            </Menu.ItemGroup>
+            <Menu.ItemGroup title="Item 2">
+              <Menu.Item key="setting:3">Option 3</Menu.Item>
+              <Menu.Item key="setting:4">Option 4</Menu.Item>
+            </Menu.ItemGroup>
+          </SubMenu>
+          <Menu.Item key="alipay">
+            <a
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "#45ab67", fontSize: "1.2em" }}
+            >
+              Daily Deals
+            </a>
+          </Menu.Item>
+        </Menu>
+      </div>
+
+      <div className="Logo" style={{ width: "10%" }}>
+        <ShoppingCartOutlined style={{ fontSize: "40px", marginTop: "10%" }} />
+        <span>My Cart</span>
+      </div>
+    </div>
+  );
+};
+export default NavbarIndex;
