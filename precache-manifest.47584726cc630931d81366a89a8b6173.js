c90a997fbe40a95524e1b9cd1d8f1e909b01bb42
videsh <videsh.ghodarop@iaugmentor.com>
2020-10-11 17:22:23 +0530

    Updates

---
+++ precache-manifest.47584726cc630931d81366a89a8b6173.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56eeb9ce35e72ae01923ace8359b12c",
+    "url": "/ecommerce-multivendor/index.html"
+  },
+  {
+    "revision": "4de99d11d54ecfcb8025",
+    "url": "/ecommerce-multivendor/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "d1fee6069737e71a1d56",
+    "url": "/ecommerce-multivendor/static/css/main.1feec82f.chunk.css"
+  },
+  {
+    "revision": "4de99d11d54ecfcb8025",
+    "url": "/ecommerce-multivendor/static/js/2.73213b73.chunk.js"
+  },
+  {
+    "revision": "c16f77d2d4ea9a0dd0d76e20358e64b8",
+    "url": "/ecommerce-multivendor/static/js/2.73213b73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1fee6069737e71a1d56",
+    "url": "/ecommerce-multivendor/static/js/main.8038eba7.chunk.js"
+  },
+  {
+    "revision": "5528e4345140129a0be6",
+    "url": "/ecommerce-multivendor/static/js/runtime-main.2ef180f9.js"
+  },
+  {
+    "revision": "eba725067d5a6fd74f55ff4bcafea3c1",
+    "url": "/ecommerce-multivendor/static/media/bg-breadcrumb.eba72506.jpg"
+  },
+  {
+    "revision": "87043a5ef019ea2c2d87baec7d22d8c8",
+    "url": "/ecommerce-multivendor/static/media/logo.87043a5e.png"
+  }
+]);
